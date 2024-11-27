84ab31c63535c5116f0e20123a4e968d572235b7
Katherine <f.katttherine@gmail.com>
2024-11-28 00:01:51 +0200

    + mobile adaptive

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,10 @@
 import style from "./header.module.css";
+import logo from "../../assets/game-logo.png";
 
 export default function Header() {
   return (
     <div className={style.header}>
-      <img className={style.image} src="public/game-logo.png" alt="tic-tac" />
+      <img className={style.image} src={logo} alt="tic-tac" />
       <h1>Tic-Tac-Toe</h1>
     </div>
   );
